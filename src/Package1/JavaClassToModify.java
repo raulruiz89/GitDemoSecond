8598da048479110b39b3c50d1918785a61161548
raulruiz89 <rrulas10@hotmail.com>
2025-10-06 16:23:37 -0600

    First project commit

---
+++ src/Package1/JavaClassToModify.java
@@ -0,0 +1,10 @@
+package Package1;
+
+public class JavaClassToModify {
+
+	public static void main(String[] args) {
+		//This is an initial update
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitHub_Demo {
+}
